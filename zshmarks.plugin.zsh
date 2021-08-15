3803ea96f80f8a32f1341115e69274b461cdde38
Robert Magill <r@magill.xyz>
2021-08-14 23:04:29 -0400

    zm_printformattedline: change __zm_printformattedline to __zm_parse_zm_line  * add option for --no-ouput  * this prob should be made into two separate functions, but it does work

--- zshmarks.plugin.zsh
+++ zshmarks.plugin.zsh
@@ -152,7 +152,7 @@ function zm() {
 
 		 if [[ $(echo $line |  awk -F'|' '{print $2}') == $zm_name ]]; then
 
-				printf "${RED}zmark name is already being used:\n$zm_name\t-- $(zms $zm_name)${NOCOLOR}\n"
+				printf "\n${RED}zmark name is already being used:\n$(zms $zm_name)${NOCOLOR}\n"
 
 				echo -n "Remove $zm_name?  (y/n)? "
 				 read answer
@@ -164,19 +164,14 @@ function zm() {
 
 		 elif [[ $(echo $line |  awk -F'|' '{print $1}') == $cur_dir  ]]; then
 
-				# TODO create function which takes a raw zmark line as input and ouputs formatted line or overwrites arguments passed
-				# local zm_clashed_path="${line%%|*}"
-				# 	zm_clashed_path="${zm_clashed_path/\$HOME/~}"
-				# 	zm_clashed_path_name="${line#*|}"
 				local formattedout zm_clashed_path zm_clashed_path_name
-__zm_printformattedline "$line" zm_clashed_path zm_clashed_path_name
-				# formattedout=$(__zm_printformattedline "$line" zm_clashed_path zm_clashed_path_name)
-					echo "zmarks/init.zsh: 174 zm_clashed_path: $zm_clashed_path"
-					echo "zmarks/init.zsh: 174 zm_clashed_path_name: $zm_clashed_path_name"
+				 __zm_parse_zm_line -n "$line" zm_clashed_path zm_clashed_path_name
 
 
-				printf "${RED}zmark path is already being used:\n$zm_clashed_path_name\t--  $zm_clashed_path${NOCOLOR}\n"
-				# printf "${RED}zmark path is already being used:\n$formattedout${NOCOLOR}\n"
+	 				printf "${RED}zmark path is already being used:\n$zm_clashed_path_name\t--  $zm_clashed_path${NOCOLOR}\n"
+				
+
+				# printf "\n${RED}zmark path is already being used:__zm_parse_zm_line $line zm_clashed_path zm_clashed_path_name${NOCOLOR}\n"
 
 				__ask_to_overwrite_zm_dir $zm_clashed_path_name $zm_name
 				return 1
@@ -258,22 +253,42 @@ function zms() {
 	 if [[ $# -eq 1 ]]; then
 			zm_name="*\|${1}"
 			zm_line=${zm_array[(r)$zm_name]}
-			__zm_printformattedline "$zm_line"
+			__zm_parse_zm_line "$zm_line"
 	 else
 			for zm_line in $zm_array; do
 				 # echo 'printing formatted line'
-				 __zm_printformattedline "$zm_line"
+				 __zm_parse_zm_line "$zm_line"
 			done
 	 fi
 }
 
 # TODO write format function for hash -d from line
-__zm_printformattedline(){
+
+# this works, but it is ugly and confusing. Prob should split this into two functions 
+__zm_parse_zm_line(){
+	 function usage {
+				   echo -e 'zmark line required\n'
+					 echo "$(basename $0) [ -n, --no-output ] zm_line [ path_variable_to_set ] [ name_variable_to_set ]"
+	 }
+
+	 if [[ "$#" -lt 1 ]]; then
+			usage
+	 fi
+
+	 local OUTPUT='true' 
+
+	 echo "\$1: $1"
+	 if [[ "$1" == '-n' || "$1" == '--no-ouput' ]]; then
+		 OUTPUT='false'
+		 shift
+	 fi
+
+	 echo "zmarks/init.zsh: 285 OUTPUT: $OUTPUT"
+
 	 local zm_line="$1"
 	 local zm_path="${zm_line%%|*}"
 	 local zm_path="${zm_path/\$HOME/~}"
 	 local zm_name="${zm_line#*|}"
-	 printf "%s\t\t--  %s\n" "$zm_name" "$zm_path"
 
 	 if [[ "$#" -gt 1 ]]; then
 			echo 'evaluating path and name'
@@ -281,6 +296,10 @@ __zm_printformattedline(){
 				 eval "$3=\"$zm_name\""
 	 fi
 
+	 if [[ OUTPUT == 'true' ]];then
+			echo 'zm_printformattedline: OUTPUT: true'
+	 	 printf "%s\t\t--  %s\n" "$zm_name" "$zm_path"
+	 fi
 }
 
 # remove a zm
@@ -314,6 +333,7 @@ function zmrm()  {
 
 						# generate new named dir to sync with marks
 						hash -d -r  # rebuild hash table
+						echo 'rebuild hash table'
 						_gen_zmarks_named_dirs 1> /dev/null
 						_gen_zmarks_named_files 1> /dev/null
 						echo "Deleted and synced named hashes"
